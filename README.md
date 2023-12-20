eb1ae8d8092e4a2793aea5ee7cb85ccf60cf91ff
Teshane Crawford <crawfordteshane@gmail.com>
2023-12-20 14:16:30 -0500

    :tada: first commit
    Initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jamaica
+
+Jamaica is a web application which highlight and showcase the best of Jamaica. It is a platform for Jamaicans to share their experiences and for visitors to learn about the island.
---
+++ app.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>
+      Jamaica is an island country situated in the Caribbean Sea. Spanning
+      10,990 square kilometres (4,240 sq mi) in area, it is the third-largest
+      island of the Greater Antilles and the Caribbean (after Cuba and
+      Hispaniola).[15] Jamaica lies about 145 kilometres (90 mi) south of Cuba,
+      and 191 kilometres (119 mi) west of Hispaniola (the island containing the
+      countries of Haiti and the Dominican Republic); the British Overseas
+      Territory of the Cayman Islands lies some 215 kilometres (134 mi) to the
+      north-west.
+    </h1>
+  </div>
+</template>
+
+<style>
+html,
+body,
+#__nuxt {
+  height: 100dvh;
+  margin: 0;
+  padding: 0;
+}
+
+html.dark {
+  background: #222;
+  color: #fff;
+}
+</style>
